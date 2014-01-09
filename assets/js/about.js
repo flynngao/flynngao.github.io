e52821dd57a832ceaac8147814d3417a23083df9
linzhi.gao <linzhi.gao@alipay.com>
2014-01-09 21:24:46 +0800

    v 0.0.01

--- assets/js/about.js
+++ assets/js/about.js
@@ -14,29 +14,51 @@ function rotate(object, degrees) {
 // each sceneshow;
 var sceneshow = [
   // first
-  function  (inpos) {
-console.log(inpos,"1")
+  function(inpos) {
+    console.log(inpos, "1")
   },
   // seconde
-  function  (inpos) {
-console.log(inpos,"2")
+  function(inpos) {
+    console.log(inpos, "2")
   },
   // third
-  function  (inpos) {
-console.log(inpos,"3")
+  function(inpos) {
+    console.log(inpos, "3")
   },
   // forth
-  function  (inpos) {
-console.log(inpos,"4")
+  function(inpos) {
+    
   },
   // end
-  function  (inpos) {
-console.log(inpos,"5")
-  },
-];
+  function(inpos) {
+    console.log(inpos)
+    if (inpos > 0.03) {
+      $($('#forthScene .fly .Galaxian-sc').get().reverse()).each(function(index) {
+              var self = this;
+          setTimeout(function() {
+              $(self).animate({
+                  "margin-left": (index + 1) * 50 + "px"
+                }
+              )
 
-var emailshow = function  () {
-  
+          }, index * 200);
+
+      });
+
+  }
+
+},
+    ];
+
+var emailshow = function() {
+  $("#preScene .startTitle,#endpreScene .startTitle").animate({
+    top: "30%"
+  }, function() {
+    $('#preScene .email,#endpreScene .email').fadeIn(500)
+    $('#preScene .tops,#endpreScene .tops').animate({
+      top: "-100px"
+    })
+  })
 };
 
 var first = false;
@@ -49,29 +71,36 @@ var eventhadle = {
     var times = 180;
     var deg = ((turn - pos) * times < 90) && pos > 0 ? -((turn - pos) * times + (pos - 1) * 90) : -(pos) * 90;
     rotate($('.scenes'), deg);
-    if (pageYOffset > 21000){
-      window.scrollTo(0,0); 
-      first = true;
+
+    if (pageYOffset > 20900) {
+      window.scrollTo(0, 0);
+      if (!first) {
+        first = true;
+        emailshow();
+      }
+
     }
+    console.log(pageYOffset);
     // background color
-    if(pos>2){
-        $('.page').addClass('pagereturn');
-    }else{
-        $('.page').removeClass('pagereturn');
+    if (pos > 2) {
+      $('.page').addClass('pagereturn');
+    } else {
+      $('.page').removeClass('pagereturn');
     }
     // chapter rotate
-    rotate($('.chapter'),360-deg);
+
+    rotate($('.chapter'), 360 - deg);
     // sceneshow
-    sceneshow[pos](turn-pos);
+    sceneshow[pos](turn - pos);
 
   },
   resize: function() {
     var w = $(window).width();
     // first edge
-    $(".scenes .citybg").css("padding-left",-$('.scenes').offset().left);
+    $(".scenes .citybg").css("padding-left", -$('.scenes').offset().left);
 
     // 
-    $("#endpreScene").css("left",(21000-$('.scenes').offset().left));
+    $("#endpreScene").css("left", (20900 - $('.scenes').offset().left));
 
     // doms which needs whole window view
     $('#preScene,#endScene .gameover,#endScene .andover,#endpreScene').css('width', w);
@@ -82,46 +111,56 @@ var eventhadle = {
 
 // person Action
 var tu_action = {
-   jump:function  () {
-     // body...
-   }
+  jump: function() {
+    // body...
+  }
 };
-document.addEventListener("touchmove", function  () {
+document.addEventListener("touchmove", function() {
   $('.scenes .citybg').css('left', -pageYOffset);
   eventhadle.scroll();
 }, false);
 
 
-window.onscroll = function() {
-  $('.scenes .citybg').css('left', -pageYOffset);
-  eventhadle.scroll();
 
-};
-
-window.onresize = function() {
-  eventhadle.resize();
+window.onload = function  () {
+   main.preload();
 };
 
 var main = {
 
   preload: function() {
-
+       $('.loading').animate({
+        top:"-100%"
+        },2000,function  (argument) {
+            main.start();
+        });
   },
   underground: function() {
-    $('.underground').each(function () {
-        for (var i = 41 ; i >= 0; i--) {     
-            $(this).append("<div class='Galaxian-sc'></div>");
-        }
+    $('.underground').each(function() {
+      for (var i = 41; i >= 0; i--) {
+        $(this).append("<div class='Galaxian-sc'></div>");
+      }
     });
   },
-  init: function() {
+  start: function() {
+    
+    window.onscroll = function() {
+      $('.scenes .citybg').css('left', -pageYOffset);
+      eventhadle.scroll();
+
+    };
+
+    window.onresize = function() {
+      eventhadle.resize();
+    };
+    
+  },init:function  () {
     var self = this
     $(document).ready(function() {
-        eventhadle.resize();
-        self.underground();
+          eventhadle.resize();
+          self.underground();
     });
   }
 };
-
 
 main.init();
